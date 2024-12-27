9af3a03a8a3dbf6f56c08247efb247bc75ff7bab
mnmnaveed <44447969+mnmnaveedshadow@users.noreply.github.com>
2024-12-27 15:29:15 +0530

    asf

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
                 <div class="d-md-flex d-block align-items-center justify-content-between my-4 page-header-breadcrumb">
                     <div class="">
                         <h1 class="page-title fw-semibold fs-20 mb-0">Empty</h1>
-                        <div class="">asfasf
+                        <div class="">
                             <nav>
                                 <ol class="breadcrumb mb-0">
                                     <li class="breadcrumb-item"><a href="#">Empty</a></li>
